da9216813fba57c47eb5548c8f2b12bc69552ba1
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2024-10-31 17:28:25 +0100

    fix: use correct import for appylar sdk

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:flutter_appylar_sdk/flutter_appylar_sdk.dart';
-import 'package:flutter_appylar_sdk/flutter_appylar_view.dart';
+import 'package:flutter_appylar/flutter_appylar.dart';
+import 'package:flutter_appylar/flutter_appylar_view.dart';
 import 'dart:io' show Platform;
 
 
